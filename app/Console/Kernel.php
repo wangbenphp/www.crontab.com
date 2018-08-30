578afb2dd42a4096977c24e23136b55217a9bd64
wangben <benwang@hk01.com>
2018-08-30 17:28:01 +0800

    crontab mysql

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\MysqlBackUpToOss;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,8 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        //MySQL定时备份到阿里云OSS
+        $schedule->job(new MysqlBackUpToOss())->everyMinute();
     }
 
     /**
